056bf1b9fbc9d5767d6cffc597e18116df627ea5
Umesh Doriya <umeshdoriyapatidar@gmail.com>
2022-07-28 18:32:48 +0530

    add production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  onfig.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address => '127.0.0.1', 
+  :port => 1025, 
+  :user_name => 'umeshdoriyapatidar@gmail.com',
+  :password => 'wnzuhdwqybisqzdb',
+  :authentication => 'plain',
+  :enable_starttls_auto => true
+  }
+  config.action_mailer.raise_delivery_errors = false
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
