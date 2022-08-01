d534c63687bc13297f47f1ba01db7273f98484fc
Umesh Doriya <umeshdoriyapatidar@gmail.com>
2022-08-01 13:41:11 +0530

    delete concern file

--- app/models/concerns/userrole.rb
+++ app/models/concerns/userrole.rb
@@ -1,14 +1,2 @@
 # frozen_string_literal: true
 
-Module Userrole
-  include ActiveSupport::Concern
-  included do 
-    def employer?
-      role == 'employer'
-    end
-
-    def applicant?
-      role == 'applicant'
-    end
-  end
-end
